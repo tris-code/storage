cdd4cde191ed909a82eaac6169841b2c513bfd51
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-09-12 23:37:42 +0000

    Initial Server implementation

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ import PackageDescription
 let package = Package(
     name: "Storage",
     products: [
+        .library(name: "Server", targets: ["Server"]),
         .library(name: "Storage", targets: ["Storage"]),
     ],
     dependencies: [
@@ -25,7 +26,7 @@ let package = Package(
             url: "https://github.com/tris-foundation/time.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/file.git",
+            url: "https://github.com/tris-foundation/aio.git",
             .branch("master")),
         .package(
             url: "https://github.com/tris-foundation/json.git",
@@ -34,10 +35,29 @@ let package = Package(
             url: "https://github.com/tris-foundation/messagepack.git",
             .branch("master")),
         .package(
+            url: "https://github.com/tris-foundation/fiber.git",
+            .branch("master")),
+        .package(
+            url: "https://github.com/tris-foundation/http.git",
+            .branch("master")),
+        .package(
+            url: "https://github.com/tris-foundation/log.git",
+            .branch("master")),
+        .package(
             url: "https://github.com/tris-foundation/test.git",
             .branch("master"))
     ],
     targets: [
+        .target(
+            name: "Server",
+            dependencies: [
+                "Storage",
+                "Time",
+                "File",
+                "Log",
+                "HTTP",
+                "MessagePack"
+            ]),
         .target(
             name: "Storage",
             dependencies: [
@@ -47,6 +67,9 @@ let package = Package(
                 "JSON",
                 "MessagePack"
             ]),
+        .testTarget(
+            name: "StorageServerTests",
+            dependencies: ["Test", "Server", "Fiber"]),
         .testTarget(
             name: "StorageTests",
             dependencies: ["Test", "Storage"]),
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,10 @@
 import XCTest
 
 import StorageTests
+import StorageServerTests
 
 var tests = [XCTestCaseEntry]()
 tests += StorageTests.__allTests()
+tests += StorageServerTests.__allTests()
 
 XCTMain(tests)
