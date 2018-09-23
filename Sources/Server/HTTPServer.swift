cb6f7f283a920e74db369ed4bd4ed5aaec6d4326
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-09-23 00:43:40 +0000

    [Server] Refactoring

--- Sources/Server/HTTPServer.swift
+++ Sources/Server/HTTPServer.swift
@@ -9,30 +9,36 @@
  *                                                                            *
  ******************************************************************************/
 
-import Test
-import File
+import Log
 import HTTP
-import Fiber
-import Stream
-import MessagePack
-import struct Foundation.UUID
+import Async
 
-@testable import Async
-@testable import Server
+final class HTTPServer {
+    var server: HTTP.Server
+    var storage: StorageProtocol
 
-extension String: Swift.Error {}
-
-final class ServerTests: TestCase {
-    let temp = Path(string: "/tmp/ServerTests")
-
-    override func setUp() {
-        async.setUp(Fiber.self)
+    init(for storage: StorageProtocol, at host: String, on port: Int) throws {
+        self.storage = storage
+        self.server = try HTTP.Server(host: host, port: port)
+        self.server.route(get: "/call/:string", to: httpHandler)
     }
 
-    func testServer() {
-        scope {
-            let storage = try Storage(at: temp.appending(#function))
-            assertNotNil(try Server(for: storage))
+    func httpHandler(
+        request: Request,
+        function: String) throws -> Response
+    {
+        let arguments = request.url.query?.values ?? [:]
+        guard let result = try storage.call(function, with: arguments) else {
+            throw HTTP.Error.notFound
         }
+        return try Response(body: result)
+    }
+
+    func start() throws {
+        try server.start()
+    }
+
+    func onError(_ error: Swift.Error) {
+        Log.critical(String(describing: error))
     }
 }
--- Sources/Storage/StorageProtocol.swift
+++ Sources/Storage/StorageProtocol.swift
@@ -9,30 +9,9 @@
  *                                                                            *
  ******************************************************************************/
 
-import Test
-import File
-import HTTP
-import Fiber
-import Stream
-import MessagePack
-import struct Foundation.UUID
-
-@testable import Async
-@testable import Server
-
-extension String: Swift.Error {}
-
-final class ServerTests: TestCase {
-    let temp = Path(string: "/tmp/ServerTests")
-
-    override func setUp() {
-        async.setUp(Fiber.self)
-    }
-
-    func testServer() {
-        scope {
-            let storage = try Storage(at: temp.appending(#function))
-            assertNotNil(try Server(for: storage))
-        }
-    }
+public protocol StorageProtocol {
+    func call(
+        _ function: String,
+        with arguments: [String : String]
+    ) throws -> Encodable?
 }
