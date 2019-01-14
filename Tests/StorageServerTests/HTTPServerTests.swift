ee843f3e3b555bb9d365ac00a67671979ffa93aa
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 22:27:40 +0000

    [Cleanup] Delete copyright notice

--- Tests/StorageServerTests/HTTPServerTests.swift
+++ Tests/StorageServerTests/HTTPServerTests.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 import Test
 import File
 import HTTP
@@ -100,7 +89,7 @@ final class HTTPServerTests: TestCase {
 
         async.task {
             defer { async.loop.terminate() }
-            _ = serverStarted.read()  
+            _ = serverStarted.read()
             scope {
                 let client = HTTP.Client(host: "localhost", port: 4002)
                 let response = try client.get(path: "/call/test?username=test")
